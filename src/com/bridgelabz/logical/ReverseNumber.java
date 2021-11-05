8f0c98d2ccde78a22aaa577ff73464a80b960c12
vsinky <sinkykumariverma@gmail.com>
2021-11-05 20:54:20 +0530

    Added reverse number

---
+++ src/com/bridgelabz/logical/ReverseNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.logical;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	public static void main(String args[]) {
+		int num = 0;
+		int reversenum = 0;
+		System.out.println("Enter the number: ");
+		Scanner in = new Scanner(System.in);
+		num = in.nextInt();
+
+		for (; num != 0;) {
+			reversenum = reversenum * 10;
+			reversenum = reversenum + num % 10;
+			num = num / 10;
+		}
+
+		System.out.println("Reverse of specified number is: " + reversenum);
+	}
+}
